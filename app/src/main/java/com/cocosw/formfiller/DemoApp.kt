29621a60225f81cde8496b5318c34bed51dd94f5
Kai <soarcn@gmail.com>
2020-07-19 12:08:17 +1000

    add more fields to the demo form

--- app/src/main/java/com/cocosw/formfiller/DemoApp.kt
+++ app/src/main/java/com/cocosw/formfiller/DemoApp.kt
@@ -8,7 +8,6 @@ class DemoApp : Application() {
     override fun onCreate() {
         super.onCreate()
         if (BuildConfig.DEBUG)
-
             FormFiller.Builder(this)
                 .keyCode(KeyEvent.KEYCODE_F)
                 .doubleTap()
@@ -16,10 +15,16 @@ class DemoApp : Application() {
                 .bullet {
                     id(R.id.username, "username")
                     id(R.id.password, "password")
+                    id(R.id.confimrpassword, "password")
+                    id(R.id.name, "Form filler")
+                    id(R.id.dob, "19/07/2020")
+                    id(R.id.address, "Sydney,Australia")
+
                 }
                 .bullet("Unhappy") {
                     id(R.id.username, "wrong")
                     id(R.id.password, "wrong")
+                    id(R.id.confimrpassword, "wrong")
                 }
                 .build()
     }
